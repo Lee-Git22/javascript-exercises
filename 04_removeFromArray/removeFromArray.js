31f04f7deacbe4f6c1ebadc743d22ae69fc28a81
Lee-Git22 <lee.alan9801@gmail.com>
2022-11-30 20:26:55 -0500

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...targets) {
 
+    for (let i = 0; i < targets.length; i++)
+    {
+        for (let j = 0; j < array.length; j++) 
+        {
+            if (array[j] === targets[i]) 
+            {
+                array.splice(j, 1);
+            }
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
