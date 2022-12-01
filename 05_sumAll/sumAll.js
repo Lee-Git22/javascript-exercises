c035d8a79131b8cd92dc272684936bb89a6c2fa7
Lee-Git22 <lee.alan9801@gmail.com>
2022-11-30 21:43:15 -0500

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    
+    if ( typeof(num1) !== "number" || typeof(num2) !== "number" || num1 < 1 || num2 < 1 ){
+        return "ERROR"
+    }
 
+    if (num1 < num2){
+        for (i = num1; i <= num2; i++){
+            sum += i;
+        }
+    }
+    else {
+        for (i = num2; i <= num1; i++){
+            sum += i;
+        }
+    }
+
+    return sum
 };
 
 // Do not edit below this line
