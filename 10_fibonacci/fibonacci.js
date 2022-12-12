8e6aa563bfb5daf9ba59cdf72ebc65be8fc54419
Lee-Git22 <lee.alan9801@gmail.com>
2022-12-12 12:21:30 -0500

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    if (input < 0) {
+        return "OOPS"
+    }
+    
+    input = parseInt(input);
 
+    const answer = [1, 1];
+    for (i = 2; i < input; i++){
+        answer[i] = answer[i - 1] + answer[i - 2];
+    }
+    return answer[input - 1];
 };
 
 // Do not edit below this line
