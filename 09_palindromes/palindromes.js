8076d5143f57ae53b35da5734e41c3ccc77093cb
Lee-Git22 <lee.alan9801@gmail.com>
2022-12-12 11:58:22 -0500

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    function isLetter(c) {
+        return (/[a-zA-Z]/).test(c)
+    }
 
+    let text1 = "";
+    let text2 = ""; 
+
+    for (i = 0; i < string.length; i++) {
+        if (isLetter(string.charAt(i))){
+            text1 += string.charAt(i).toLowerCase();
+            text2 = string.charAt(i).toLowerCase() + text2; 
+        }
+    }
+
+    if (text1 == text2){
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
