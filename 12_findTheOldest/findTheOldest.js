2072abad2d1e1e366730b138e18ec78bbd1723a3
Lee-Git22 <lee.alan9801@gmail.com>
2022-12-12 15:33:33 -0500

    Finish exercuse 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let today = new Date();
+    const year = today.getFullYear();
 
-};
+    const oldest = people.sort(function(a, b) {
+        if (!(a.hasOwnProperty("yearOfDeath"))) {
+            a.yearOfDeath = (year);
+        }
+
+        if(!(b.hasOwnProperty("yearOfDeath"))) {
+            b.yearOfDeath = (year);
+        }
+
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    });
+    return oldest[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
