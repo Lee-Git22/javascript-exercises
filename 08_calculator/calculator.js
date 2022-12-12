11be0aabaa1989970eebe2ab4c5b776b7240906d
Lee-Git22 <lee.alan9801@gmail.com>
2022-12-12 11:17:50 -0500

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return parseInt(x + y);
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return parseInt(x - y);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let initialSum = 0;
+  let sumWithInitial = array.reduce((total, current) => total + current, initialSum);
+  return sumWithInitial;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let initialProduct = 1;
+  let productWithInitial = array.reduce((total, current) => total * current, initialProduct);
+  return productWithInitial;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return parseInt(x ** y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let answer = 1;
+  const count = x;
+  for (i = 1; i <= x; i++) {
+    answer *= x - (x - i)
+  }
+  return parseInt(answer)
 };
 
 // Do not edit below this line
