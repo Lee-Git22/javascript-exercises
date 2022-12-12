ed9d656cb2502735b453aebbdc55eed0c3f2def9
Lee-Git22 <lee.alan9801@gmail.com>
2022-12-12 12:34:27 -0500

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let i = 0;
+    list = [];
+    for (title in books){
+        list.push(books[i]["title"]);
+        i++;
+    }
+    return list;
 };
 
 // Do not edit below this line
